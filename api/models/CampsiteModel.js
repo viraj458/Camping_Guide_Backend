89860c12c7b338af9c742aa0d04938c0685fd1fc
Viraj <virajmalaka94@gmail.com>
2023-01-03 19:05:50 +0530

    update CampsiteModel

--- api/models/CampsiteModel.js
+++ api/models/CampsiteModel.js
@@ -1,4 +1,10 @@
-const mongoose = require('mongoose');
+var mongoose = require("mongoose");
+var bcrypt = require('bcrypt');
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+
+const SALT = 10;
+
 const Schema = mongoose.Schema;
 
 const CampsiteSchema = new Schema({
@@ -10,7 +16,20 @@ const CampsiteSchema = new Schema({
         type: String,
         required: true
       },
+      business_registration_number:{
+        type: String,
+        required: true,
+        unique: true
+      },
       descripton:{
+        type: String,
+        required: true
+      },
+      phone_number:{
+        type: String,
+        required: true
+      },
+      password:{
         type: String,
         required: true
       },
@@ -23,6 +42,68 @@ const CampsiteSchema = new Schema({
         required: false
       },
     });
+
+
+//saving campsite data
+
+    CampsiteSchema.pre('save',function(next){
+      var campsite = this;
+      if(campsite.isModified('password')){
+          //checking if password field is available and modified
+          bcrypt.genSalt(SALT,function(err,salt){
+              if(err) return next(err)
+  
+              bcrypt.hash(campsite.password,salt,function(err,hash){
+                  if (err) return next(err)
+                  campsite.password = hash;
+                  next();
+              });
+          });
+      }else{
+          next();
+      }
+  });
+
+
+  //For comparing the campsites entered password with database during login
+CampsiteSchema.methods.comparePassword = function (candidatePassword,callBack){
+  bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+      if(err) return callBack(err);
+      callBack(null,isMatch);
+  }); 
+
+  };
+
+
+//For generating token when loggedin
+CampsiteSchema.methods.generateToken = function(callBack){
+  var campsite =this;
+  var token = jwt.sign(campsite._id.toHexString(),process.env.SECRETE);
+  
+  callBack(null,token);
+};
+
+//validating token for auth routes middleware
+CampsiteSchema.statics.findByToken = function(token,callBack){
+  jwt.verify(token, process.env.SECRETE,function(err,decode){
+      //this decode must give user_id if token is valid .ie decode = user_id
+      Campsite.findById(decode,function(err,campsite){
+          if(err){
+              
+              return res.status(404).json({
+                  sucess:false,
+                  message:"Invalid Campsite ID!",
+                  data:err
+                
+              });
+          }
+
+          callBack(null,campsite);
+      });
+  });
+};
+
+
     
     const Campsite =mongoose.model('Campsite',CampsiteSchema);
     module.exports = {Campsite}
